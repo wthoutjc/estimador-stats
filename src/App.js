e6ef20b60b64b19c24738ea953a26666e2995655
wthoutjc <juancamilorr2012@hotmail.com>
2021-09-26 21:30:16 -0500

    Adding Production

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 //import './css/App.css';
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
 //MathJAX
 import { MathComponent } from "mathjax-react";
@@ -12,28 +18,26 @@ import ICMuestraGrande from "./Temario/ICMuestraGrande.js";
 import ICMuestraPequena from "./Temario/ICMuestraPequena.js";
 import ICSigma from "./Temario/ICSigma.js";
 
-
 //Componentes EstimadorPuntual:
-import EPApp1 from './components/c_ep/App1.js';
+import EPApp1 from "./components/c_ep/App1.js";
 import EPApp2 from "./components/c_ep/App2.js";
 import EPApp3 from "./components/c_ep/App3.js";
 import EPApp4 from "./components/c_ep/App4.js";
 
 //Componentes ICMuestraGrande:
-import ICMGApp1 from './components/c_icmg/App1.js';
+import ICMGApp1 from "./components/c_icmg/App1.js";
 import ICMGApp2 from "./components/c_icmg/App2.js";
 import ICMGApp3 from "./components/c_icmg/App3.js";
 import ICMGApp4 from "./components/c_icmg/App4.js";
 
 //Componentes ICMuestraPequeña:
-import ICMPApp1 from './components/c_icmp/App1.js';
+import ICMPApp1 from "./components/c_icmp/App1.js";
 import ICMPApp2 from "./components/c_icmp/App2.js";
 
 //css
-import './index.css'
+import "./index.css";
 
 class App extends Component {
-
   state = {
     raiz: false,
   };
@@ -42,77 +46,80 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-        <Route path="/icmp/app1">
-            <ICMPApp1/>
+          <Route path="/icmp/app1">
+            <ICMPApp1 />
           </Route>
           <Route path="/icmp/app2">
-            <ICMPApp2/>
+            <ICMPApp2 />
           </Route>
           <Route path="/icmg/app1">
-            <ICMGApp1/>
+            <ICMGApp1 />
           </Route>
           <Route path="/icmg/app2">
-            <ICMGApp2/>
+            <ICMGApp2 />
           </Route>
           <Route path="/icmg/app3">
-            <ICMGApp3/>
+            <ICMGApp3 />
           </Route>
           <Route path="/icmg/app4">
-            <ICMGApp4/>
+            <ICMGApp4 />
           </Route>
           <Route path="/ep/app1">
-            <EPApp1/>
+            <EPApp1 />
           </Route>
           <Route path="/ep/app2">
-            <EPApp2/>
+            <EPApp2 />
           </Route>
           <Route path="/ep/app3">
-            <EPApp3/>
+            <EPApp3 />
           </Route>
           <Route path="/ep/app4">
-            <EPApp4/>
+            <EPApp4 />
           </Route>
           <Route path="/ep">
-            <EstimadorPuntual/>
+            <EstimadorPuntual />
           </Route>
           <Route path="/icmg">
-            <ICMuestraGrande/>
+            <ICMuestraGrande />
           </Route>
           <Route path="/icmp">
-            <ICMuestraPequena/>
+            <ICMuestraPequena />
           </Route>
           <Route path="/ics">
-            <ICSigma/>
+            <ICSigma />
           </Route>
           <Route exact path="/">
             <div className="App">
               <table className="table table-striped table-hover table-bordered my-1">
-
                 {/* CABEZA */}
 
                 <thead className="thead-dark">
                   <tr>
                     <th scope="col">TÍTULO</th>
-                    <th scope="col" className="text-center">CONTENIDO</th>
+                    <th scope="col" className="text-center">
+                      CONTENIDO
+                    </th>
                   </tr>
                 </thead>
 
                 {/* CUERPO: Apps 1-4*/}
-                
-                <tbody>
 
-                  {/* App 1: */} 
+                <tbody>
+                  {/* App 1: */}
 
                   <tr>
                     <th scope="col">
-                      <p className="h6">cap.8.3: tabla.8.1 & cap.8.4</p> Evaluación de la bondad de un estimador puntual.
+                      <p className="h6">cap.8.3: tabla.8.1 & cap.8.4</p>{" "}
+                      Evaluación de la bondad de un estimador puntual.
                     </th>
                     <td>
                       <Link to="/ep">
-                        <button 
-                          type="button" 
+                        <button
+                          type="button"
                           className="btn btn-sm btn-success"
-                        >IR</button>
+                        >
+                          IR
+                        </button>
                       </Link>
                     </td>
                   </tr>
@@ -121,14 +128,17 @@ class App extends Component {
 
                   <tr>
                     <th scope="col">
-                    <p className="h6">cap.8.6</p>Intervalos de confianza en una muestra grande
+                      <p className="h6">cap.8.6</p>Intervalos de confianza en
+                      una muestra grande
                     </th>
                     <td>
                       <Link to="/icmg">
-                        <button 
-                          type="button" 
+                        <button
+                          type="button"
                           className="btn btn-sm btn-success"
-                        >IR</button>
+                        >
+                          IR
+                        </button>
                       </Link>
                     </td>
                   </tr>
@@ -137,16 +147,24 @@ class App extends Component {
 
                   <tr>
                     <th scope="col">
-                    <p className="h6">cap.8.8</p>
-                      <p>Intervalos de confianza de una muestra pequeña para </p>
-                      <p><MathComponent tex={String.raw`\mu \ \ y \ \ \mu_1 - \mu_2\ `} /></p>
+                      <p className="h6">cap.8.8</p>
+                      <p>
+                        Intervalos de confianza de una muestra pequeña para{" "}
+                      </p>
+                      <p>
+                        <MathComponent
+                          tex={String.raw`\mu \ \ y \ \ \mu_1 - \mu_2\ `}
+                        />
+                      </p>
                     </th>
                     <td>
                       <Link to="/icmp">
-                        <button 
-                          type="button" 
+                        <button
+                          type="button"
                           className="btn btn-sm btn-success"
-                        >IR</button>
+                        >
+                          IR
+                        </button>
                       </Link>
                     </td>
                   </tr>
@@ -155,16 +173,20 @@ class App extends Component {
 
                   <tr>
                     <th scope="col">
-                    <p className="h6">cap.8.9</p>
+                      <p className="h6">cap.8.9</p>
                       <p>Intervalos de confianza para </p>
-                      <p><MathComponent tex={String.raw`\sigma^2\ `} /></p>
+                      <p>
+                        <MathComponent tex={String.raw`\sigma^2\ `} />
+                      </p>
                     </th>
                     <td>
                       <Link to="/ics">
-                        <button 
-                          type="button" 
+                        <button
+                          type="button"
                           className="btn btn-sm btn-success"
-                        >IR</button>
+                        >
+                          IR
+                        </button>
                       </Link>
                     </td>
                   </tr>
@@ -173,7 +195,7 @@ class App extends Component {
             </div>
           </Route>
           <Route path="*">
-            <h1> 404 Not Found </h1>
+            <Redirect to="/" />
           </Route>
         </Switch>
       </Router>
